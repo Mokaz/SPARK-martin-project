47ff5274139599ff261d824458701d01a40fcf1f
Mokaz <martiniuss.wan@gmail.com>
2025-01-24 16:50:59 -0500

    Added transform from nav_cam_link to fcu_link WIP

--- src/spark_softdrone/launch/mavros_t265_vio_launch.py
+++ src/spark_softdrone/launch/mavros_t265_vio_launch.py
@@ -9,7 +9,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     realsense_launch_dir = os.path.join(
         get_package_share_directory('realsense2_camera'), 'launch'
@@ -18,13 +17,20 @@ def generate_launch_description():
         get_package_share_directory('mavros'), 'launch'
     )
     mavros_px4_config_dir = os.path.join(
-        get_package_share_directory('spark_softdrone'), 'launch', 'px4_config_spark.yaml'
+        get_package_share_directory('spark_softdrone'), 'launch'
+    )
+    robot_state_publisher_launch_dir = os.path.join(
+        get_package_share_directory('spark_softdrone'), 'launch'
     )
 
     realsense_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(realsense_launch_dir, 'rs_launch.py')
-        )
+        ), 
+        launch_arguments={
+            'enable_fisheye1': 'false',
+            'enable_fisheye2': 'false',
+        }.items()
     )
 
     mavros_launch = IncludeLaunchDescription(
@@ -33,7 +39,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'fcu_url': 'serial:///dev/ttyTHS1:921600',
-            'config_yaml': mavros_px4_config_dir
+            'config_yaml': os.path.join(mavros_px4_config_dir, 'px4_config_spark.yaml'),
         }.items()
     )
 
@@ -44,8 +50,15 @@ def generate_launch_description():
         output='screen',
     )
 
+    robot_state_publisher_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(robot_state_publisher_launch_dir, 'robot_state_publisher.py')
+        )
+    )
+
     return LaunchDescription([
+        robot_state_publisher_launch,
         mavros_launch,
         realsense_launch,
-        t265_to_mavros_node
+        t265_to_mavros_node,
     ])
--- src/spark_softdrone/src/t265_to_mavros.cpp
+++ src/spark_softdrone/src/t265_to_mavros.cpp
@@ -2,12 +2,17 @@
 #include <nav_msgs/msg/odometry.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 class T265ToMavrosNode : public rclcpp::Node
 {
 public:
     T265ToMavrosNode()
-    : Node("t265_to_mavros_node")
+    : Node("t265_to_mavros_node"),
+      tf_buffer_(this->get_clock()),
+      tf_listener_(tf_buffer_)
     {
         // Subscribe to T265 Odometry
         sub_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
@@ -37,13 +42,46 @@ public:
 private:
     void odomCallback(const nav_msgs::msg::Odometry::SharedPtr odom_msg)
     {
+        // Lookup transform from nav_cam_link to fcu_link
+        geometry_msgs::msg::TransformStamped transform_stamped;
+        try {
+            transform_stamped = tf_buffer_.lookupTransform(
+                "fcu_link",         // target
+                "nav_cam_link",     // source
+                tf2::TimePointZero
+            );
+        } catch (tf2::TransformException &ex) {
+            RCLCPP_WARN(this->get_logger(), "%s", ex.what());
+            return;
+        }
+
+        // Transform pose
+        geometry_msgs::msg::PoseStamped pose_in, pose_out;
+        pose_in.header = odom_msg->header;
+        pose_in.pose   = odom_msg->pose.pose;
+        tf2::doTransform(pose_in, pose_out, transform_stamped);
+
+        // Transform twist (only rotates vectors)
+        geometry_msgs::msg::Vector3Stamped lin_in, lin_out;
+        geometry_msgs::msg::Vector3Stamped ang_in, ang_out;
+        lin_in.header = odom_msg->header;
+        lin_in.vector = odom_msg->twist.twist.linear;
+        ang_in.header = odom_msg->header;
+        ang_in.vector = odom_msg->twist.twist.angular;
+        tf2::doTransform(lin_in, lin_out, transform_stamped);
+        tf2::doTransform(ang_in, ang_out, transform_stamped);
+
+        // Fill transformed messages
         geometry_msgs::msg::PoseWithCovarianceStamped pose_cov_msg;
-        pose_cov_msg.header = odom_msg->header;
-        pose_cov_msg.pose = odom_msg->pose;
+        pose_cov_msg.header           = pose_out.header;
+        pose_cov_msg.pose.pose        = pose_out.pose;
+        pose_cov_msg.pose.covariance  = odom_msg->pose.covariance;
 
         geometry_msgs::msg::TwistWithCovarianceStamped twist_cov_msg;
-        twist_cov_msg.header = odom_msg->header;
-        twist_cov_msg.twist = odom_msg->twist;
+        twist_cov_msg.header                  = pose_out.header;
+        twist_cov_msg.twist.twist.linear      = lin_out.vector;
+        twist_cov_msg.twist.twist.angular     = ang_out.vector;
+        twist_cov_msg.twist.covariance        = odom_msg->twist.covariance;
 
         pub_pose_cov_->publish(pose_cov_msg);
         pub_twist_cov_->publish(twist_cov_msg);
@@ -55,6 +93,10 @@ private:
     // Publishers
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_pose_cov_;
     rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr pub_twist_cov_;
+
+    // TF2
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 
 int main(int argc, char * argv[])
